b8020c5a064b822b9faa574d36e6840d40a58f22
amiribenabdellah.personal <amir.iben-abdellah@nexum.com>
2022-04-08 09:51:38 +0200

    test

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,10 @@ import { babel } from '@rollup/plugin-babel';
 
 const config = {
   input: 'src/app.js',
-
+  output: {
+	file: 'dist/bundle.js',
+    format: 'iife'
+  },
   plugins: [babel({ babelHelpers: 'bundled' })]
 };
 
