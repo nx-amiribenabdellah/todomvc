061f67377d165b2efc0c67d63ae26ac2a3e2ecb6
nx-amiribenabdellah <amir.iben-abdellah@nexum.com>
2022-04-13 10:23:53 +0200

    test

--- lighthouserc.js
+++ lighthouserc.js
@@ -5,12 +5,7 @@ module.exports = {
             staticDistDir: './',
             numberOfRuns: 3
         },
-        assert: {
-            assertions: {
-                'categories:performance': ['warn', {minScore: 1}],
-                'categories:accessibility': ['error', {minScore: 1}]
-            }
-        },
+
         upload: {
             /* Add configuration here */
             target: 'temporary-public-storage'
