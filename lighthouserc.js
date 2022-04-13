8a1dd49de68cc466b09d4f882f995fcd284ab52c
nx-amiribenabdellah <amir.iben-abdellah@nexum.com>
2022-04-13 10:26:50 +0200

    test

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,10 +2,17 @@ module.exports = {
     ci: {
         collect: {
             /* Add configuration here */
-            staticDistDir: './',
+            startServerCommand: 'npm run start',
+            url: ['http://localhost:8080'],
             numberOfRuns: 3
         },
-
+        //https://web.dev/lighthouse-ci/
+       /* assert: {
+            assertions: {
+                'categories:performance': ['warn', {minScore: 1}],
+                'categories:accessibility': ['error', {minScore: 1}]
+            }
+        },*/
         upload: {
             /* Add configuration here */
             target: 'temporary-public-storage'
